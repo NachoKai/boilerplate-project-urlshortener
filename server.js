1ccaa384140f10668e770006cbcef2a094ae462b
Nacho Caiafa <nachokai@outlook.com>
2020-11-15 18:05:06 -0300

    .

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongodb = require("mongodb");
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/shorturl/new/", (req, res) => {
+  console.log(req.params);
+  res.json({
+    success: "ok!",
+  });
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
