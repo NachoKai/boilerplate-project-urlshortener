19a56234e0633f394778c18e623f5b97c1890c1f
Nacho Caiafa <nachokai@outlook.com>
2020-11-15 19:39:49 -0300

    handle redirect

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongodb = require("mongodb");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const shortId = require("shortid");
+let uuid = shortId.generate();
 const jsonParser = bodyParser.json();
-const app = express();
 const port = process.env.PORT || 3000;
 const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
 const regex = new RegExp(expression);
+const app = express();
 
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const ShortUrl = mongoose.model("ShortUrl", new mongoose.Schema({ name: String }));
+const ShortUrl = mongoose.model(
+  "ShortUrl",
+  new mongoose.Schema({
+    short_url: String,
+    original_url: String,
+    uuid: String,
+    saved: Boolean,
+  })
+);
 
 app.use(cors());
 
@@ -23,24 +34,46 @@ app.use(bodyParser.json());
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
 app.post("/api/shorturl/new/", jsonParser, (req, res) => {
   let requestedUrl = req.body.url;
-  let uuid = shortId.generate();
 
-  if (requestedUrl.match(regex)) {
-    res.json({
-      short_url: uuid,
-      original_url: requestedUrl,
-    });
-  } else {
-    res.json({ error: "invalid url" });
-  }
+  let newURL = new ShortUrl({
+    short_url: __dirname + "/api/shorturl/" + uuid,
+    original_url: requestedUrl,
+    uuid: uuid,
+  });
+
+  newURL.save((err, data) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    } else {
+      if (requestedUrl.match(regex)) {
+        res.json({
+          short_url: newURL.short_url,
+          original_url: newURL.original_url,
+          uuid: newURL.uuid,
+          saved: true,
+        });
+      } else {
+        res.json({ error: "invalid url" });
+      }
+    }
+  });
 });
 
-app.listen(port, function () {
+app.get("/api/shorturl/:uuid", (req, res) => {
+  let userGeneratedUuid = req.params.uuid;
+  ShortUrl.find({ uuid: userGeneratedUuid }).then(data => {
+    let urlForRedirect = data[0];
+    res.redirect(urlForRedirect.original_url);
+  });
+});
+
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
