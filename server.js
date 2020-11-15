5be2b4f8c8c93dba6a215e23110a50808b5a9968
Nacho Caiafa <nachokai@outlook.com>
2020-11-15 18:26:29 -0300

    .

--- server.js
+++ server.js
@@ -3,14 +3,19 @@ const express = require("express");
 const cors = require("cors");
 const mongodb = require("mongodb");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const shortId = require("shortid");
+const jsonParser = bodyParser.json();
 const app = express();
-
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
-
-// Basic Configuration
 const port = process.env.PORT || 3000;
 
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
@@ -18,16 +23,13 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post("/api/shorturl/new/", (req, res) => {
-  console.log(req.params);
+app.post("/api/shorturl/new/", jsonParser, (req, res) => {
+  let requestedUrl = req.body.url;
+  let uuid = shortId.generate();
   res.json({
-    success: "ok!",
+    short_url: uuid,
+    original_url: requestedUrl,
   });
-});
-
-// Your first API endpoint
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
 });
 
 app.listen(port, function () {
