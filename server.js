62cfd2976a70d90b1fa9bb924c897ea150c3281f
Nacho Caiafa <nachokai@outlook.com>
2020-11-20 22:16:28 -0300

    .

--- server.js
+++ server.js
@@ -47,9 +47,8 @@ app.post("/api/shorturl/new/", jsonParser, (req, res) => {
   });
 
   newURL.save((err, url) => {
-    if (err) {
-      console.error(err);
-    } else if (requestedUrl.match(regex)) {
+    if (err) console.err(err);
+    if (requestedUrl.match(regex)) {
       res.json({
         short_url: url.short_url,
         original_url: url.original_url,
@@ -69,21 +68,6 @@ app.get("/api/shorturl/:uuid", (req, res) => {
   });
 });
 
-app.route('/api/shorturl/new').post((req,res)=> {
-  ...
-    let urlMatch = // Match the URL to expected structure or not
-
-    // Lookup the matched URL
-    dns.lookup(urlMatch, (err,add,fam) => {
-      // If the URL does not exist, return expected error
-      if (err) return res.json({"error": "invalid URL"});
-   
-      // Save to database, otherwise.
-    })
-  });
-});
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
