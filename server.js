302c38269c48a5bba39ce0ba8cf1b8aa429a44dd
Nacho Caiafa <nachokai@outlook.com>
2020-11-20 22:29:43 -0300

    .

--- server.js
+++ server.js
@@ -2,21 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
 const bodyParser = require("body-parser");
 const dns = require("dns");
-
 const mongoose = require("mongoose");
+const port = process.env.PORT || 3000;
 const { Schema } = mongoose;
+
 const connection = mongoose.createConnection(process.env.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const autoIncrement = require("mongoose-auto-increment");
-autoIncrement.initialize(connection);
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
+const autoIncrement = require("mongoose-auto-increment");
+
+autoIncrement.initialize(connection);
 
 app.use(cors());
 
@@ -26,37 +25,37 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-// Solution (Author: Pratik Var)
 const urlSchema = new Schema({
   url: String,
 });
+
 urlSchema.plugin(autoIncrement.plugin, { model: "URL", field: "urlID" });
+
 const URL = connection.model("URL", urlSchema);
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.post("/api/shorturl/new", (req, res) => {
-  // check if valid URL
   const reqURL = req.body.url;
-  let urlMatch =
-    reqURL.match(/(?<=(http|https):\/\/)[^\/]+/) &&
-    reqURL.match(/(?<=(http|https):\/\/)[^\/]+/)[0];
+  const regEx = /(?<=(http|https):\/\/)[^\/]+/;
+  let urlMatch = reqURL.match(regEx) && reqURL.match(regEx)[0];
+
   urlMatch = !urlMatch ? "url-to-fail" : urlMatch;
+
   dns.lookup(urlMatch, (err, add, fam) => {
     if (err) res.json({ error: "invalid URL" });
     else {
-      // check if URL already exists in database
       URL.findOne({ url: req.body.url }).exec((err, doc) => {
         if (err) return console.error(err);
         if (doc !== null) {
           res.json({ original_url: req.body.url, short_url: doc.urlID });
         } else {
-          // if not, create new record for URL
           let urlObj = new URL({ url: req.body.url });
+
           urlObj.save(err => {
             if (err) return console.error(err);
             res.json({ original_url: req.body.url, short_url: urlObj.urlID });
@@ -66,6 +65,7 @@ app.post("/api/shorturl/new", (req, res) => {
     }
   });
 });
+
 app.get("/api/shorturl/:id", (req, res) => {
   URL.findOne({ urlID: req.params.id }).exec((err, doc) => {
     if (err) return console.error(err);
