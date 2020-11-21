867f76bc5fc771f70933859c03289c7c1ab5125f
Nacho Caiafa <nachokai@outlook.com>
2020-11-20 22:20:13 -0300

    .

--- server.js
+++ server.js
@@ -19,43 +19,37 @@ app.listen(port, function () {
   console.log("Node.js listening on port " + port);
 });
 
-/*
-app.get("/", function(req, res) {
-    console.log("pasó por /");
-    //http://stackoverflow.com/questions/17014384/how-to-render-multiple-ejs-files-in-nested-form-in-node-js-and-express
-    res.render('index.html');
-    //mod.render(res, 'views', "index.html", []);
+app.get("/", function (req, res) {
+  console.log("pasó por /");
+  res.render("index.html");
+  mod.render(res, "views", "index.html", []);
 });
-app.get("/lessons", function(req, res) {
-    console.log(req.url);
-    console.log("pasó por lessons");
-    
-    res.render('lessons.html', function(err, html) {
-        if(err) {
-            console.log(err);
-        }
-        res.send(html);
-    });
+app.get("/lessons", function (req, res) {
+  console.log(req.url);
+  console.log("pasó por lessons");
+
+  res.render("lessons.html", function (err, html) {
+    if (err) {
+      console.log(err);
+    }
+    res.send(html);
+  });
 });
-*/
 
 app.get("*", function (req, res) {
-  //var usageReq = false;
+  var usageReq = false;
   var urlPassed = req.url;
-  urlPassed = urlPassed.substring(1, urlPassed.length); //remove first "/"
+  urlPassed = urlPassed.substring(1, urlPassed.length);
 
-  /*
-    if(urlPassed.length > 6 && urlPassed.substring(0, 5) == "usage") { //seee if usage asked in order to render answer
-        //console.log("detectó usage");
-        usageReq = true;
-        urlPassed = urlPassed.substring(6, urlPassed.length) 
-        
-    }
-    */
+  if (urlPassed.length > 6 && urlPassed.substring(0, 5) == "usage") {
+    console.log("detectó usage");
+    usageReq = true;
+    urlPassed = urlPassed.substring(6, urlPassed.length);
+  }
 
-  //console.log(urlPassed);
-  var expected = mod.seeIfExpected(urlPassed); //returns url(if url) or num(if number) or na(if it doesn't match any)
-  //console.log(expected);
+  console.log(urlPassed);
+  var expected = mod.seeIfExpected(urlPassed);
+  console.log(expected);
   mod.processInfo(expected, urlPassed, function (answer) {
     console.log("showing answer");
     console.log(answer);
@@ -68,35 +62,29 @@ app.get("*", function (req, res) {
         error: answer.info,
       });
     } else {
-      /*
-            if (usageReq == true) {
-                //do something different, if entered number, go to page associated, else render and link strings
-                if (answer.found == false) { //means it was an insertion, lets render answer and link it
-                //http://stackoverflow.com/questions/10919650/accessing-express-js-local-variables-in-client-side-javascript
-                    res.render('render.html', answer);
-                
-                } else { //means it was found
-                    res.render('render.html', answer);
-                }
-            } else {
-                */
-      if (answer.jsonRes == true) {
-        res.json({
-          long_url: answer.url,
-          short_url: answer.id,
-        });
+      if (usageReq == true) {
+        if (answer.found == false) {
+          res.render("render.html", answer);
+        } else {
+          res.render("render.html", answer);
+        }
       } else {
-        // go to url
-        //if(/https/g.test(answer.url)) {
-        res.redirect(answer.url);
-        /*} else {
-                        console.log("le falta https");
-                        var newUrl = "https://" + answer.url;
-                        console.log(newUrl);
-                        res.redirect(newUrl);
-                    }*/
+        if (answer.jsonRes == true) {
+          res.json({
+            long_url: answer.url,
+            short_url: answer.id,
+          });
+        } else {
+          if (/https/g.test(answer.url)) {
+            res.redirect(answer.url);
+          } else {
+            console.log("le falta https");
+            var newUrl = "https://" + answer.url;
+            console.log(newUrl);
+            res.redirect(newUrl);
+          }
+        }
       }
-      //}
     }
   });
 });
